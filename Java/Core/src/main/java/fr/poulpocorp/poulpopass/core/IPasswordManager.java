f570cb73f4377cd534344f5498eb7aaaee7ba097
PoulpoGaz <valent.carrez@gmail.com>
2020-11-28 18:12:18 +0100

    structure test 3

--- Java/Core/src/main/java/fr/poulpocorp/poulpopass/core/IPasswordManager.java
+++ Java/Core/src/main/java/fr/poulpocorp/poulpopass/core/IPasswordManager.java
@@ -1,20 +1,6 @@
 package fr.poulpocorp.poulpopass.core;
 
-import java.nio.file.Path;
-
 public interface IPasswordManager {
-
-    void open(Path file, char[] masterPassword) throws PasswordManagerException;
-
-    void open(Path file, char[] masterPassword, boolean closeIfOpen) throws PasswordManagerException;
-
-    boolean addCategory(Category category);
-
-    boolean removeCategory(Category category);
-
-    boolean addPassword(Password password);
-
-    boolean removePassword(Password password);
 
     int getNumberOfCategories();
 
@@ -23,10 +9,4 @@ public interface IPasswordManager {
     char[] getMasterPassword();
 
     void setMasterPassword(char[] masterPassword);
-
-    void close() throws PasswordManagerException;
-
-    void close(Path out) throws PasswordManagerException;
-
-    boolean isOpen();
 }
