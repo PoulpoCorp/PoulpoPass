cce8b6d1ffcc49993a82ede444f3e789006b929e
PoulpoGaz <valent.carrez@gmail.com>
2021-01-03 18:06:27 +0100

    default frame

---
+++ Java/App/src/main/java/fr/poulpocorp/poulpopass/app/App.java
@@ -0,0 +1,19 @@
+package fr.poulpocorp.poulpopass.app;
+
+import javax.swing.*;
+
+public class App extends JFrame  {
+
+    public App() {
+        super("PoulpoPass");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initComponents();
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private void initComponents() {
+
+    }
+}
--- Java/App/src/main/java/fr/poulpocorp/poulpopass/app/Main.java
+++ Java/App/src/main/java/fr/poulpocorp/poulpopass/app/Main.java
@@ -1,8 +1,19 @@
 package fr.poulpocorp.poulpopass.app;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLaf;
+
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        FlatLaf.registerCustomDefaultsSource("theme");
+        FlatDarculaLaf.install();
 
+        EventQueue.invokeLater(() -> {
+            App app = new App();
+            app.setVisible(true);
+        });
     }
 }
